d0e40fa1ae97e5bb9ca97614a2e72b3dfd1cb194
yanachen1314 <2742015729@qq.com>
2023-02-03 17:55:46 +0800

    新增test.txt资源和inetText测试类

---
+++ src/test/java/com/yan/inetAddressTest/inetTest.java
@@ -0,0 +1,11 @@
+package com.yan.inetAddressTest;
+
+public class inetTest {
+/**
+ * @Author          烟尘
+ * @CreateDate      2023/2/3 17:52
+ * @Version         1.0
+ * @Description     test
+ *
+ */
+}
